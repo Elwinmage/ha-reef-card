87ee797c470d1c5f31612961f2ca2b4bbea7e10a
Elwinmage <moranderic31@gmail.com>
2025-12-02 20:07:56 +0100

    Sort supplement list

--- scripts/supplement_img.py
+++ scripts/supplement_img.py
@@ -2,26 +2,34 @@
 
 import os
 import re
+from operator import itemgetter
+
 from supplements_list import SUPPLEMENTS
+
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 supplement_list="## Supplements\nHere is the list of supported images for the supplement. If yours as a ❌, you can request its addition [here](https://github.com/Elwinmage/ha-reef-card/discussions/25).\n"
+supplement_list+="<table>"
+supplements_sorted=sorted(SUPPLEMENTS, key=lambda d: d['fullname'])
 
-for supplement in SUPPLEMENTS:
+for supplement in supplements_sorted:
     uid=supplement['uid']
     try:
-        open(dir_path+"/../src/devices/img/"+uid+".supplement.png")
-        supplement_list+="- ✅"
+        filename=dir_path+"/../src/devices/img/"+uid+".supplement.png"
+        with open(filename):
+            supplement_list+="<tr><td>✅</td>"
+            supplement_list+="<td>"+supplement['fullname']+"</td>"
+            supplement_list+="<td><img style='width:20%;' src='src/devices/img/"+uid+".supplement.png'/></td></tr>"
     except:
-
-        supplement_list+="- ❌"
-    supplement_list+=supplement['fullname']+"\n"
+        supplement_list+="<tr><td> ❌</td>"
+        supplement_list+="<td colspan='2'>"+supplement['fullname']+"</td></tr>"
+supplement_list+="</table>\n"
 supplement_list+="\n# ReefLed"
 
 with open (dir_path+"/../README.md") as f:
     data = f.read()
-    d= re.sub(r"## Supplements([^\$]+)# ReefLed",supplement_list,data,re.M| re.DOTALL)
+    d= re.sub(r"## Supplements([^\$]+)# ReefLed",supplement_list,data,re.M | re.DOTALL)
 with open (dir_path+"/../README.md","w") as f:
     f.write(d)
 
