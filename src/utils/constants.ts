4eae299d7997682d7228bb78a536679a7d24604f
Elwinmage <moranderic31@gmail.com>
2026-02-13 07:40:59 +0100

    centralize color definition in ./utils/constant.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Global constants used across the application
+ */
+
+/**
+ * Color used when device/element is OFF or disabled
+ * RGB format: "R,G,B"
+ */
+export const OFF_COLOR = "150,150,150";
+
+/**
+ * Default alpha (opacity) value for colors
+ */
+export const DEFAULT_ALPHA = 0.5;
